363e2f0b39b0f9bfa20015f4f24e88f15beb2118
Mau Ulisse Luz <maulisseluz@gmail.com>
2025-02-12 06:48:58 -0300

    feat: add sanitize body of contact request, add new api key for postmark

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
+const rateLimit = require("express-rate-limit")
+const sanitizeHtml = require("sanitize-html")
 const postmark = require('postmark')
 
 const postmark_client = process.env.POSTMARK
 
-async function getHello(_req, res) {
-    return res.status(200).json({ message: 'Hello World!' })
-}
+//* This regex will catch potential malicious scripts in the body.
+const maliciousJsRegex = /(<script\b[^>]*>[\s\S]*?<\/script>|javascript\s*:|on\w+\s*=|eval\s*\(|document\.(cookie|write|location)|window\.(location|open)|fetch\s*\(|XMLHttpRequest\s*\()/i
 
 async function postNewContact(req, res, next) {
 
@@ -13,30 +14,43 @@ async function postNewContact(req, res, next) {
 
     if (!name || !email || !message) return res.status(400).json({ error: 'You must provide name, email and message parameters before submitting.' })
 
-    const client = new postmark.ServerClient(postmark_client)
+    if (maliciousJsRegex.test(name) || maliciousJsRegex.test(email) || maliciousJsRegex.test(message)) {
+        return res.status(400).json({ error: "Invalid input detected." });
+    }
 
-    await client.sendEmail({
-        "From": "mauluz@symetria.lat",
-        "To": "mauluz@symetria.lat",
-        "Subject": "Mau, you have a new message!",
-        "HtmlBody": `<div>
-                <strong>This one comes from</strong> ${name}.
+    try {
+
+        //* Let's clean the inputs from malicious tags.
+        const safeName = sanitizeHtml(name)
+        const safeEmail = sanitizeHtml(email)
+        const safeMessage = sanitizeHtml(message)
+
+        const client = new postmark.ServerClient(postmark_client)
+
+        await client.sendEmail({
+            "From": "mauluz@symetria.lat",
+            "To": "mauluz@symetria.lat",
+            "Subject": "Mau, you have a new message!",
+            "HtmlBody": `<div>
+                <strong>This one comes from</strong> ${safeName}.
                 <br>
-                <p>This is their email: ${email}</p>
+                <p>This is their email: ${safeEmail}</p>
                 <br>
-                <p>And this is their message: ${message}</p>
+                <p>And this is their message: ${safeMessage}</p>
                 <br>
                 <p>If you have any questions, feel free to contact us at <a href="mailto:support@symetria.lat">support@symetria.lat</a></p>
                 </div>`,
-        "TextBody": "Mau, you have a new message!",
-        "MessageStream": "outbound"
-    })
+            "TextBody": "Mau, you have a new message!",
+            "MessageStream": "outbound"
+        })
 
-    res.status(201).json({ message: 'Email sent succesfully.' })
+        res.status(201).json({ message: 'Email sent succesfully.' })
+    } catch (error) {
+        next(error)
+    }
 }
 
 
 module.exports = {
-    getHello,
     postNewContact
 }
